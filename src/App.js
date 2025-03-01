52fbe648166778f9d7fb62466952984604409ea5
jjingofarouk <jjingofaroukk@gmail.com>
2025-02-28 22:19:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
+// App.js
 import React from 'react';
-import './style.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-export default function App() {
+// Importing components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
+import Testimonials from './components/Testimonials';
+import DonationForm from './components/DonationForm';
+import ContactForm from './components/ContactForm';
+
+// Importing pages
+import Home from './pages/Home';
+import About from './pages/About';
+import OurWork from './pages/OurWork';
+import GetInvolved from './pages/GetInvolved';
+import Donate from './pages/Donate';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Terms from './pages/Terms';
+
+// Importing styles
+import './styles/global.css';
+import './styles/navbar.css';
+import './styles/footer.css';
+import './styles/home.css';
+
+const App = () => {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Router>
+      <div>
+        {/* Navbar Component */}
+        <Navbar />
+
+        {/* Hero Section on Home page */}
+        <Route exact path="/" component={HeroSection} />
+
+        {/* Main content */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/our-work" component={OurWork} />
+          <Route path="/get-involved" component={GetInvolved} />
+          <Route path="/donate" component={Donate} />
+          <Route path="/blog" component={Blog} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/privacy-policy" component={PrivacyPolicy} />
+          <Route path="/terms" component={Terms} />
+        </Switch>
+
+        {/* Footer Component */}
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
+
+export default App;
